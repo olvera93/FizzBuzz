10675d7cfe07bf14d63b7c5a7fbf2e47b1b93df5
olvera93 <olvera9302@outlook.com>
2023-04-12 17:08:58 -0600

    Initial commit

---
+++ FizzBuzz/FizzBuzzApp.swift
@@ -0,0 +1,17 @@
+//
+//  FizzBuzzApp.swift
+//  FizzBuzz
+//
+//  Created by Gonzalo Olvera Monroy on 12/04/23.
+//
+
+import SwiftUI
+
+@main
+struct FizzBuzzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
